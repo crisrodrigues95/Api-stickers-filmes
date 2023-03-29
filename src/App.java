b6e4eb55aff4f3857f898a3d13d76510c037e230
Cristhian Rodrigues <cristhian.rodrigues95@gmail.com>
2023-03-29 03:46:53 -0300

    Aula 2

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -12,12 +14,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         
 
-        // Fazer uma conexão HTTP
+        // Fazer uma conexão HTTp
 
-        
-
-
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
@@ -37,16 +36,26 @@ public class App {
 
         
         for (Map<String, String> filme : listadeFilmes){
+
+            String urlimage = filme.get("image");
+
+            InputStream inputStream = new URL(urlimage).openStream();
+
+            String imageName = ((filme.get("title")).replace(":", "-")+".png");
+
+            var generator = new StickerGenerator();
+            double stars = Double.parseDouble((filme.get("imDbRating")));
+            generator.creating(inputStream, imageName, stars);
+
+            System.out.println("#"+filme.get("rank")+":");
             System.out.println(UI.negrito("Título: ")+filme.get("title"));
-            System.out.println(UI.negrito("Poster: ") + filme.get("image"));
-            double stars = Double.parseDouble((filme.get("imDbRating"))) ;
             System.out.println(UI.color(stars)+("Classificação: ")+ stars+UI.ANSI_RESET) ;
             for (int i = 1; i<= Math.round(stars); i++){
                 System.out.print("⭐");
 
             }
-            System.out.println();
-            System.out.println();
+            System.out.println("\n");
+            
             
 
         }
